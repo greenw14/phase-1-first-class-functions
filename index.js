0a6b768a968112de28c9012a53c36aea5ef6d868
william <willhgreen02@gmail.com>
2021-07-21 09:41:57 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback,spy) {
+    return callback(spy)
+}
+function returnsANamedFunction(){
+return function namedfunction(){
+
+}
+   
+}
+function returnsAnAnonymousFunction(){
+  return function (){
+      
+  }
+}
